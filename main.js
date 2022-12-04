1d28ce36ce671aa1cb1732bbf5c5c1f3b276503f
M868-2 <113375937+M868-2@users.noreply.github.com>
2022-12-04 16:35:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-// JavaScript Code
+// JavaScript 
+// EVENT LISTENER
+document.getElementById("btn").addEventListener("click" , btnclicked);
+
+// VARIABLES
+let outputEl = document.getElementById("output");
+let q1 = document.getElementById("qnOne");
+let q2 = document.getElementById("qnTwo").toLowerCase();
+let q3 = document.getElementById("qnThree").toLowerCase();
+let q4 = document.getElementById("qnFour").toLowerCase();
+let q5 = document.getElementById("qnFive");
+
+// IF STATEMENTS
+function btnclicked() {
+    let score = 0 
+    if (q1 === 7) {
+        score+20;
+    } else if (q2 === "bangtan sonyeodan") {
+        score+20;
+    } else if (q3 === "June 12, 2013" || q3 === "June 12 2013") {
+        score+20
+    } else if (q4 === "no more dream") {
+        score+20;
+    } else if (q5 === 462) {
+        score+20
+    }
+}
